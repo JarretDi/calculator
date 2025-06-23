c0e6e23b862b89c4dea4cc9bba04a5bf1e5fe4d5
Jarret Diep <jarrettcdi@gmail.com>
2025-06-23 14:44:11 -0600

    add input first number functionality
    -add display and updater to calc.js
    -add event listener to numbers to input
       -note: works with (-) and . in correct places
    -slightly modified index.html to work w/ JS

--- calc.js
+++ calc.js
@@ -1,13 +1,36 @@
+let state = 1;
+/*
+    state should be one-of:
+    1
+    2
+    ERROR
+*/
+
 let n1;
 let op;
 let n2;
+
+const display = document.querySelector(".display");
+
+function updateDisplay() {
+    switch (state) {
+        case 1:
+            display.textContent = n1 ? n1 : "";
+            break;
+        case 2:
+            display.textContent = n2 ? n2 : "";
+            break;
+        case "ERROR":
+            display.textContent = "ERROR";
+    }
+}
 
 function roundTo3(num) {
     return Math.round(num * 1000) / 1000;
 }
 
 function add(n1, n2) {
-    return roundTo3(n1 + n2);
+    return roundTo3(+n1 + +n2);
 }
 
 function sub(n1, n2) {
@@ -27,6 +50,49 @@ function operate() {
         return "ERROR";
     }
 
-    // update display?
+    n1 = op(n1, n2);
+    op = undefined;
+    n2 = undefined;
+    state = 1;
+
+    updateDisplay();
+
     return op(n1, n2);
 }
+
+const numbers = document.querySelector(".numbers");
+numbers.addEventListener("click", (e) => {
+    const num = e.target.className;
+    if (num === "numbers") return;
+
+    if (state === 1) {
+        if (n1 === undefined) {
+            if (num === ".") {
+                n1 = "0.";
+            } else {
+                n1 = num
+            }
+        } else {
+            if (num === "." && !n1.includes(".")) {
+                n1 += num;
+            } else if ("0123456789".includes(num)) {
+                n1 += num;
+            }
+        }
+    } else if (state === 2) {
+        if (n2 === undefined) {
+            if (num !== ".") {
+                n2 = num;
+            } else {
+                n2 = "0.";
+            }
+        } else {
+            if (num === "." && !n2.includes(".")) {
+                n2 += num;
+            } else if ("0123456789".includes(num)) {
+                n2 += num;
+            }
+        }
+    }
+    updateDisplay();
+});
